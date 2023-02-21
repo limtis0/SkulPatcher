9dc0a452a62e9e5cb148b3e4fa623ffcc7791454
Vladimir <limtis2109@gmail.com>
2023-02-21 01:02:23 +0100

    Initial commit

---
+++ SkulPatcher/Main.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace SkulPatcher
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Main : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
